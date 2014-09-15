85416201fc7cb71e34195b8bf6e3a4c3e77935be
DrewWeth <dgwetherington@gmail.com>
2014-09-15 15:07:13 -0500

    precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(:default, Rails.env)
 
 module Texting
   class Application < Rails::Application
-    
+    config.assets.enabled = true  
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +23,5 @@ module Texting
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
   end
 end
